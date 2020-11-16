63150944fe82f5e9589612fde0100a447798d85a
Vasu Neelapala <vasu.yo@gmail.com>
2020-11-16 08:41:55 -0500

    Adding sm initial config.

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-11-15T09:44:06-0500",
+    date = "2020-11-16T08:33:27-0500",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 13.0.2 (Oracle Corporation)"
 )
 @Component
