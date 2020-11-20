ec2cff7061a01d9d95166fcaca6c4eeb82d5c7e5
Vasu Neelapala <vasu.yo@gmail.com>
2020-11-20 07:42:28 -0500

    Initiate allocation request

--- src/main/java/guru/sfg/beer/order/service/services/BeerOrderManagerImpl.java
+++ src/main/java/guru/sfg/beer/order/service/services/BeerOrderManagerImpl.java
@@ -43,6 +43,9 @@ public class BeerOrderManagerImpl implements BeerOrderManager{
 
         if(isValid){
             sendBeerOrderEvent(beerOrder, BeerOrderEventEnum.VALIDATION_PASSED);
+            BeerOrder validatedOrder = beerOrderRepository.findOneById(beerOrderId);
+
+            sendBeerOrderEvent(validatedOrder, BeerOrderEventEnum.ALLOCATE_ORDER);
         } else {
             sendBeerOrderEvent(beerOrder, BeerOrderEventEnum.VALIDATION_FAILED);
         }
