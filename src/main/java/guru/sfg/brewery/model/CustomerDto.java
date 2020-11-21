278ed9c194ecd0db1949d24c49bfed23b0a6f481
Vasu Neelapala <vasu.yo@gmail.com>
2020-11-21 09:54:56 -0500

    adding list customers end point.

--- src/main/java/guru/sfg/brewery/model/CustomerDto.java
+++ src/main/java/guru/sfg/brewery/model/CustomerDto.java
@@ -43,6 +43,6 @@ public class CustomerDto {
     @JsonProperty("lastModifiedDate")
     private OffsetDateTime lastModifiedDate = null;
 
-    private String name;
+    private String customerName;
 }
 
