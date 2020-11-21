371654eccfadcd580ad887c3227d40efdb4170f6
Vasu Neelapala <vasu.yo@gmail.com>
2020-11-21 09:32:41 -0500

    Adding Deallocate Order Action.

--- src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -22,6 +22,7 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
     private final Action<BeerOrderStatusEnum, BeerOrderEventEnum>  allocateOrderAction;
     private final Action<BeerOrderStatusEnum, BeerOrderEventEnum>  validationFailureAction;
     private final Action<BeerOrderStatusEnum, BeerOrderEventEnum>  allocationFailureAction;
+    private final Action<BeerOrderStatusEnum, BeerOrderEventEnum>  deallocateOrderAction;
 
     @Override
     public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
@@ -77,8 +78,9 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
                 .event(BeerOrderEventEnum.BEERORDER_PICKED_UP)
                 .and().withExternal()
                 .source(BeerOrderStatusEnum.ALLOCATED).target(BeerOrderStatusEnum.CANCELLED)
-                .event(BeerOrderEventEnum.CANCEL_ORDER);
-        //todo add action;
+                .event(BeerOrderEventEnum.CANCEL_ORDER)
+                .event(BeerOrderEventEnum.CANCEL_ORDER)
+                .action(deallocateOrderAction);
 
     }
 }
